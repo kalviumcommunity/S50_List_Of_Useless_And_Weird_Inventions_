24129021b44c279bb1d115e859e9604c943d9a32
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2024-02-15 15:24:34 +0530

    CRUD Operation

--- Routes/Post.js
+++ Routes/Post.js
@@ -9,9 +9,73 @@ router.get("/posts", async (req, res) => {
         const data = await PostModel.find();
         res.json(data);
     } catch (error) {
-        console.log(error); 
+         console.log(error); 
+    }
+});
+
+//Post request
+
+router.post("/posts", async (req, res) => {
+    try {
+        const data = await PostModel.create(req.body);
+        console.log(data)
+        res.json(data);
+    } catch (error) {
+        console.log(error);
         res.status(500).send("An error occurred");
     }
 });
+
+
+//Put request
+
+router.put("/posts/:id", async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const updatedPost = await PostModel.findByIdAndUpdate(postId, req.body, { new: true });
+        if (!updatedPost) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+        res.json(updatedPost);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+
+
+// PATCH request
+
+router.patch("/posts/:id", async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const updatedFields = req.body;
+        const updatedPost = await PostModel.findByIdAndUpdate(postId, updatedFields, { new: true });
+        if (!updatedPost) {
+            return res.status(404).json({ error: "Post not found" });
+        }
+        res.json(updatedPost);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("An error occurred");
+    }
+});
+
+
+//Delete request
+
+router.delete("/posts/:id", async (req, res)=>{
+    const id = req.params.id
+    try{
+        const deletedpost = await postModel.findByIdAndDelete(id);
+        if (!deletedUser) {
+          return res.status(404).json({ error: "User not found" });
+        }
+        res.status(200).json({ msg: 'Deleted successfully' });
+      } catch (error) {
+        res.status(500).json({ error: error.message });
+      }
+})
 
 module.exports = router;
