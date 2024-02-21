6a070dc920f53dac32c41820fd6cdd823314163e
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2024-02-21 08:39:45 +0530

    Fetched locally

--- FrontEnd/Useless_Inventions/src/Components/DummyPost.jsx
+++ FrontEnd/Useless_Inventions/src/Components/DummyPost.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -18,7 +19,7 @@ function DummyPosts() {
     <>
         <div className="ml-10 mt-10 mb-10">
       {posts.map((post) => (
-        <div key={post.id}>
+        <div key={post.id}>``
           <p className='font-bold mt-12'>{post.Title}</p>
           <p>{post.Inventor}</p>
           <p>{post.InventedYear}</p>
