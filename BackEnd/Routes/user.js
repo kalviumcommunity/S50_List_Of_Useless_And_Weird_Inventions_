97662bc39eec0c3958572046ee865a2bd568cbb6
Kopperun Silambazhagii <silambazhagii@Silambazhagiis-MacBook-Pro-M1.local>
2024-02-27 10:37:08 +0530

    Updated

--- BackEnd/Routes/user.js
+++ BackEnd/Routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router(); 
 const userModel = require('../Schema/UserModel');
+const Joi = require('joi');
+const userSchema = require('./UserSchema'); 
 
 //Get request
-
 router.get("/users", async (req, res) => {
     try {
         const data = await userModel.find();
@@ -15,15 +16,16 @@ router.get("/users", async (req, res) => {
 });
 
 //Post request
-
 router.post("/users", async (req, res) => {
     try {
-        console.log(req.body)
-        const {Username, Nickname, Email, Password} = req.body.data;
+        const { error, value } = userSchema.validate(req.body);
+        if (error) {
+            return res.status(400).send(error.details[0].message);
+        }
 
-        console.log(Username, Nickname, Email, Password)
-        const data = await userModel.create({Username, Nickname, Email, Password});
-        
+        const { Username, Nickname, Email, Password } = value;
+
+        const data = await userModel.create({ Username, Nickname, Email, Password });
         res.json(data);
     } catch (error) {
         console.log(error);
@@ -32,7 +34,6 @@ router.post("/users", async (req, res) => {
 });
 
 //Put request
-
 router.put("/users/:id", async (req, res) => {
     try {
         const userId = req.params.id;
@@ -48,36 +49,33 @@ router.put("/users/:id", async (req, res) => {
 });
 
 //Patch request 
-
-router.put("/users/:id", async (req, res) =>{
+router.patch("/users/:id", async (req, res) =>{
     try {
         const userId = req.params.id;
         const updatedFields = req.body;
-        const updatedUser = await PostModel.findByIdAndUpdate(userId, updatedFields, { new: true });
+        const updatedUser = await userModel.findByIdAndUpdate(userId, updatedFields, { new: true });
         if (!updatedUser) {
-            return res.status(404).json({ error: "Post not found" });
+            return res.status(404).json({ error: "User not found" });
         }
-        res.json(updatedPost);
+        res.json(updatedUser);
     } catch (error) {
         console.error(error);
         res.status(500).send("An error occurred");
     }
-})
-
+});
 
 //Delete request
-
 router.delete("/users/:id", async (req, res)=>{
-    const id = req.params.id
-    try{
+    const id = req.params.id;
+    try {
         const deletedUser = await userModel.findByIdAndDelete(id);
         if (!deletedUser) {
-          return res.status(404).json({ error: "User not found" });
+            return res.status(404).json({ error: "User not found" });
         }
         res.status(200).json({ msg: 'Deleted successfully' });
-      } catch (error) {
+    } catch (error) {
         res.status(500).json({ error: error.message });
-      }
-})
+    }
+});
 
 module.exports = router;
